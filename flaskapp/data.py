180c2f6e3e9ebe427169105eed47ad5e887465c8
Elisabeth <32246797+elisabethmarie@users.noreply.github.com>
2019-03-06 18:54:21 +0100

    add files

---
+++ flaskapp/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum tenetur dolorem dignissimos voluptas ipsum? Facilis itaque accusantium necessitatibus deleniti amet molestias, dicta rem dignissimos nesciunt ipsam asperiores consectetur maiores commodi.',
+            'author': 'Eli',
+            'create_date': '06-03-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum tenetur dolorem dignissimos voluptas ipsum? Facilis itaque accusantium necessitatibus deleniti amet molestias, dicta rem dignissimos nesciunt ipsam asperiores consectetur maiores commodi.',
+            'author': 'Elisa',
+            'create_date': '06-03-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum tenetur dolorem dignissimos voluptas ipsum? Facilis itaque accusantium necessitatibus deleniti amet molestias, dicta rem dignissimos nesciunt ipsam asperiores consectetur maiores commodi.',
+            'author': 'Elsi',
+            'create_date': '06-03-2019'
+        }
+    ] 
+    return articles
